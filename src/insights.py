d28212c649ce7052d55a88015938fe4bac577fda
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-08-04 17:43:15 -0300

    :sparkles: feat: Implementa função filter_by_industry
    retorna os jobs por industry

--- src/insights.py
+++ src/insights.py
@@ -30,21 +30,12 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    jobs_by_industry = []
+    for job in jobs:
+        industry_of_job = job["industry"]
+        if industry_of_job == industry:
+            jobs_by_industry.append(job)
+    return jobs_by_industry
 
 
 def get_max_salary(path):
