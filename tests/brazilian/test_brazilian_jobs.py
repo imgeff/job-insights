d1d5071e834a58b5194f9f8fb510659b77c7f180
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-08-05 00:25:37 -0300

    :white_check_mark: test(unit): Implementa teste da função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,10 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    path = "tests/mocks/brazilians_jobs.csv"
+    brazilian_jobs_in_english = read_brazilian_file(path)
+    keys_in_portuguese = ["titulo", "salario", "tipo"]
+    for job in brazilian_jobs_in_english:
+        for key in keys_in_portuguese:
+            assert key not in job
